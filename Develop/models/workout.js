d0bc0cec2766ca7a0cc3eef354ff0fe61869be9f
Nish Mitchell <shanishamit@gmail.com>
2021-08-07 18:33:36 -0400

    adding the framework

---
+++ Develop/models/workout.js
@@ -0,0 +1,35 @@
+//track the name, type, weight, sets, reps, and duration of exercise. 
+//If the exercise is a cardio exercise, I should be able to track my distance traveled.
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction"
+  },
+  weight: {
+    type: Number,
+    required: "Enter an amount"
+  },
+  sets: {
+    type: Number,
+    required: "Enter an amount"
+  },
+  duration: {
+    type: Number,
+    required: "Enter Time"
+  },
+
+  distance: {
+    type: Number,
+    required: "Enter distance"
+  }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
