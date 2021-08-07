e3d67620c6933754697efd3642ff4fc657c0f327
Nish Mitchell <shanishamit@gmail.com>
2021-08-07 18:50:37 -0400

    updated the local host route

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Transaction = require("Develop/models/workout.js");
+const Workout = require("../model/workout.js")
 
 router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
@@ -11,25 +11,25 @@ router.post("/api/transaction", ({ body }, res) => {
     });
 });
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+// router.post("/api/transaction/bulk", ({ body }, res) => {
+//   Transaction.insertMany(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+// router.get("/api/transaction", (req, res) => {
+//   Transaction.find({})
+//     .sort({ date: -1 })
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
+//figured out db name from seed script
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
