916302e3427112a760878fc9468ac0e681685940
Nish Mitchell <shanishamit@gmail.com>
2021-08-08 22:19:33 -0400

    updating

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //db name from seed script
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
