e45863630be6d0e2e01fda94cb37d2fdd4959ad9
Nish Mitchell <shanishamit@gmail.com>
2021-08-08 23:49:31 -0400

    deploying

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //db name from seed script
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
-       useCreateIndex: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
